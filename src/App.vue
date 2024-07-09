7722ef6a144174ee258a843f68ede6d7dd47a8dd
ATM-Shifat <shifatatm@gmail.com>
2024-07-09 13:33:50 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue'
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import joblistings from '@/components/JobListings.vue'
+</script>
+
+<template>
+  <NavBar/>
+  <Hero />
+  <HomeCards/>
+  <!-- <HomeCards bg_color="bg-red-100"/> -->
+   <joblistings />
+</template>
